4081483878b687efc0aae4370fb3a489874d8c80
lodjim <lodjim5@gmail.com>
2023-11-17 18:10:51 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI,File, UploadFile
+import uvicorn
+from ultralytics import YOLO
+from PIL import Image
+import io 
+from collections import Counter
+
+class serverApi:
+    def __init__(self,host:str,port:int) -> None:
+        self.port = port
+        self.host = host
+        self.model = YOLO("./model/yolov8m.pt") 
+        self.api = FastAPI(title="Self Checkout Api")
+        self.api.add_api_route("/status",self.serve_status,methods=['GET'])
+        self.api.add_api_route("/inference",self.inference,methods=["POST"])
+
+    async def serve_status(self):
+        return {
+            "status":"alive"
+        }
+    
+    async def inference(self,file:UploadFile=File(...)):
+        image_binary = await file.read()
+        img_to_model = Image.open(io.BytesIO(image_binary))
+        results = self.model(img_to_model)
+        names = self.model.names
+        list_of_items = []
+        for r in results:
+            for c in r.boxes.cls:
+                list_of_items.append(names[int(c)])
+        print(list_of_items)
+        result = Counter(list_of_items)
+        content = dict(result)
+        return {"content":content}
+    
+    def start_server(self):
+        config = uvicorn.Config(self.api,self.host,self.port)
+        server = uvicorn.Server(config)
+        server.run()
+
+
+
+
+
+if __name__ == "__main__":
+    my_server = serverApi("localhost",8000)
+    my_server.start_server()
+
